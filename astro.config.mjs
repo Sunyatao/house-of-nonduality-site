28713e69468631d4b1b01deb15ba45df62c70ebf
Pieter Abts <de.middenweg@gmail.com>
2026-06-04 10:18:52 +0200

    deploy website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://houseofnonduality.com",
-  base: "/house-of-nonduality-site/",
   integrations: [sitemap()],
 });
